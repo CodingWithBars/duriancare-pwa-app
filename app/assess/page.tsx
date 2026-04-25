76e67e5f6b47bd8b5b4890e453d842f177497cd7
CodingWithBars <barrojohnnems01@gmail.com>
2026-04-25 11:37:53 +0800

    working model

--- app/assess/page.tsx
+++ app/assess/page.tsx
@@ -68,7 +68,7 @@ export default function AssessPage() {
     const loadModel = async () => {
       try {
         tflite.setWasmPath('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-tflite@0.0.1-alpha.10/wasm/');
-        const loadedModel = await tflite.loadTFLiteModel('/unta_kaloy_an_na_v6.tflite');
+        const loadedModel = await tflite.loadTFLiteModel('/fusion_model_float32.tflite');
         setModel(loadedModel);
         console.log("Model loaded successfully");
       } catch (err) {
