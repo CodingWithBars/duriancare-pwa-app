8b70c05043262816b91a6059cd0995abed3ae039
CodingWithBars <barrojohnnems01@gmail.com>
2026-04-25 11:59:58 +0800

    Upload Float32 model and strict Webpack CopyPlugin config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,33 @@
 import type { NextConfig } from "next";
 import withPWAInit from "@ducanh2912/next-pwa";
 
+const CopyPlugin = require("copy-webpack-plugin");
+
 const withPWA = withPWAInit({
-  dest: "public",       // The output directory for the service worker
+  dest: "public",
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  disable: false,       // Set to true if you want to disable PWA in development
+  disable: false,
   workboxOptions: {
     disableDevLogs: true,
   },
 });
 
 const nextConfig: NextConfig = {
-  // Your other Next.js config options here
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "node_modules/@tensorflow/tfjs-tflite/wasm",
+            to: "static/chunks", // This automatically resolves to .next/static/chunks/
+          },
+        ],
+      })
+    );
+    return config;
+  },
 };
 
 export default withPWA(nextConfig);
