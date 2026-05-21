0e89c8672bed468f9ae60354b7fc2e7811b5907b
CodingWithBars <barrojohnnems01@gmail.com>
2026-05-21 10:08:24 +0800

    app limit extended 28

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,4 +4,4 @@ export const SUPPORT_CONTACT = "+63 909 180 4520";
 export const SYSTEM_ID = "DC-APP-0001";
 
 // --- REMINDER CONFIG ---
-export const REMINDER_START_DAYS = 7; // Show reminder 7 days before deadline
+export const REMINDER_START_DAYS = 2; // Show reminder 7 days before deadline
